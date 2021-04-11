e06d34c3885aa231049ee91f32b25b7b6956843b
wtnm <wtnm@rambler.ru>
2021-04-11 19:01:38 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,126 @@
+import {is, isArray, isFunction, isMergeable, isString} from "../is-fns";
+import {anyObject, getIn, objKeys, objKeysNSymb, setIn, PathElement} from "../object-fns";
+
+
+interface MergeStateResult {
+  state: any,
+  changes?: any,
+}
+
+export interface MergeOptions {
+  path?: PathElement[]; // path in first object that should be merged with second object
+  replace?: replaceType; // force replace for mergeable object instead of merge, can be boolean, object with boolean props or a function of following type (path: any[], objectA: anyObject, objectB: anyObject) => boolean
+  arrays?: (path: PathElement[], arrayA: any[], arrayB: any[]) => any[]; // function to merge arrays
+  diff?: boolean; // if true then first object is changed in the way to be deeply equal to the second
+  del?: boolean;  // remove props with MergeOptions.delSymbol
+  delSymbol?: any;  // symbol that marks props that should be removed if MergeOptions.del is true
+  noSymbol?: boolean; // if true, then skips symbol props in objects
+}
+
+export type replaceType = { [key: string]: boolean | replaceType } | boolean | ((path: PathElement[], objectA: anyObject, objectB: anyObject) => boolean);
+
+
+function mergeState(state: any, source: any, options: MergeOptions = {}): MergeStateResult {
+  const fn = options.noSymbol ? objKeys : objKeysNSymb;
+  // let arrayMergeFn: any = false;
+  let {delSymbol, del, diff, replace, arrays, path} = options;
+  if (path) {
+    if (isString(path)) path = path.split('/');
+    source = setIn({}, source, path);
+    if (replace && !isFunction(replace))
+      replace = setIn({}, replace, path);
+  }
+  let forceReplace: any = replace;
+  if (typeof forceReplace !== 'function') {
+    if (!isMergeable(replace)) forceReplace = () => false;
+    else forceReplace = (path: any) => getIn(replace, path)
+  }
+  if (replace === true || forceReplace([], state, source) === true) return {state: source, changes: state !== source ? source : undefined};
+  if (!isFunction(arrays)) arrays = undefined;
+
+  function recusion(state: any, source: any, track: any[] = []): MergeStateResult {
+    const changes: any = {};
+    const isSourceArray = isArray(source);
+    if (!isMergeable(state)) {
+      state = isSourceArray ? [] : {};  // return only elements
+      if (isArray(state)) changes.length = 0;
+    }
+    const isStateArray = isArray(state);
+    if (!isMergeable(source)) return {state};  // merge only mergeable elements, may be throw here
+
+    if (isStateArray && isSourceArray) {
+      if (arrays) source = arrays(track, state, source);
+      if (state.length != source.length) changes.length = source.length;
+    }
+
+    let stateKeys = fn(state);
+    if (stateKeys.length == 0 && !del) {
+      if (!isStateArray && !isSourceArray)
+        return fn(source).length ? {state: source, changes: source} : {state};
+      if (isStateArray && isSourceArray) {
+        if (state.length == source.length && source.length == 0) return {state};
+        return (fn(source).length || source.length !== state.length) ? {state: source, changes: source} : {state};
+      }
+    }
+
+    let srcKeys = fn(source);
+
+    const changedObjects: any = {};
+    const result = (isStateArray ? [] : {});
+
+
+    if (diff) {
+      stateKeys.forEach(key => {
+        if (!~srcKeys.indexOf(key))
+          changes[key] = delSymbol;
+      });
+    }
+
+
+    srcKeys.forEach(key => {
+      if (del && source[key] === delSymbol) {
+        if (state.hasOwnProperty(key)) changes[key] = delSymbol;
+      } else {
+        let keyTrack = track.concat(key);
+        if (!isMergeable(source[key]) || !isMergeable(state[key]) || forceReplace(keyTrack, state[key], source[key]) === true) {
+          if (!state.hasOwnProperty(key) || !is(state[key], source[key])) changes[key] = source[key];
+        } else {
+          if (state[key] !== source[key]) {
+            let obj = recusion(state[key], source[key], keyTrack);
+            if (obj.changes)
+              changedObjects[key] = obj;
+          }
+        }
+      }
+    });
+
+    let changedObjKeys = fn(changedObjects);
+    let changesKeys = fn(changes);
+    if (changesKeys.length == 0 && changedObjKeys.length == 0) return {state};
+    else {
+      Object.assign(result, state);
+      changesKeys.forEach(key => {
+        if (del && changes[key] === delSymbol || diff && !source.hasOwnProperty(key)) delete result[key];
+        else result[key] = changes[key];
+      });
+      changedObjKeys.forEach(key => {
+        result[key] = changedObjects[key].state;
+        changes[key] = changedObjects[key].changes
+      });
+      return {state: result, changes}
+    }
+  }
+
+  return recusion(state, source)
+}
+
+const merge: any = (a: any, b: any, opts: MergeOptions = {}) => mergeState(a, b, opts).state;
+
+
+merge.all = function (state: any, obj2merge: any[], options: MergeOptions = {}) {
+  if (obj2merge.length == 0) return state;  // no changes should be done
+  else return obj2merge.reduce((prev, next) => merge(prev, next, options), state);  // merge
+};
+
+
+export {merge, mergeState} // react-merge
---
+++ readme.md
@@ -0,0 +1,107 @@
+
+
+<!-- toc -->
+
+
+
+<!-- tocstop -->
+
+## Overview
+`react-merge` - function that merge 2 (or more) objects in a proper "react"-way. I.e. if second object doesn't change first then the result object is strictly equals first object.
+```
+import merge from 'react-merge';
+let firstObject = {"a":1, "b": {"c":2, "d":3}}
+let result = merge(fisrtObject, {"a":1});
+console.log(result === firstObject); // true
+result = merge(fisrtObject, {"a":2, "b":{"c":2}});
+console.log(result === firstObject); // false
+console.log(result.b === firstObject.b); // true
+```
+
+
+## Installation
+
+To install the stable version:
+
+```
+npm install --save react-merge
+```
+## Usage
+```
+import {merge} from 'react-merge'
+```
+
+## Documentation
+
+#### merge(objectA: any, objectB: any, options: MergeOptions)
+Merges `objectA` with `objectB` in react-way. 
+
+If `objectB` is not mergeable then `objectA` is returned as result. 
+
+If `objectA` is not mergeable then new object deeply equal to `objectB` is returned as result. 
+
+If both are not mergeable then empty object is returned. 
+
+If `objectA` is array and `objectB` is not array then the result is array with props of `objectB`.
+
+`options` - object with following properties:
+  - `path?: string | any[]` - // path in first object that should be merged with second object
+  - `replace?: replaceType` - force replace for mergeable object instead of merge, can be boolean, object with boolean or function props or a function of the following type `(path: any[], objectA: anyObject, objectB: anyObject) => boolean`
+  - `arrays?: Function` - function to merge arrays of the following type `(path: any[], arrayA: any[], arrayB: any[]) => any[]`. Example, to concat arrays `(t, a, b) => a.concat(b)`, to replace `(t, a, b) => b`
+  - `del?: boolean` - remove props with `MergeOptions.delSymbol`
+  - `delSymbol?: any` - symbol that marks props that should be removed if MergeOptions.del is true, default is `undefined`
+  - `noSymbol?: boolean` - if true, then skips symbol props in objects
+  - `diff?: boolean` - if true then first object is changed in the way to be deeply equal to the second.
+
+#### merge.all(objectA: any, objectsB: any[], options: MergeOptions)
+Merges `objectA` with `objectsB` array.
+
+##Examples: 
+
+##### merge array with object
+```
+import merge from 'react-merge';
+let firstObject = {"a":[0, 1, 2, 3]}
+let secondObject = {"a": {"1": 11, "length":2}};
+let result = merge(fisrtObject, secondObject);
+console.log(result); // {"a": [0, 11]}
+
+```
+
+##### default array merging
+```
+import merge from 'react-merge';
+let firstObject = {"a":[0, 1, 2, 3]}
+let secondObject = {"a": []};
+secondObject.a[2] = 22;
+secondObject.a.length = 5;
+let result = merge(fisrtObject, secondObject);
+console.log(result); // {"a": [0, 1, 22, 3, undefined]}
+```
+
+##### del options
+```
+import merge from 'react-merge';
+let firstObject = {"a": 1, "b": {"c":2, "d":3}}
+let secondObject = {"a": undefined};
+let result = merge(fisrtObject, secondObject, {del:true});
+console.log(result); // {"b": {"c":2, "d":3}}
+
+result = merge(fisrtObject, secondObject, {del:false});
+console.log(result); // {"a": undefined, "b": {"c":2, "d":3}}
+```
+
+##### diff options
+```
+import merge from 'react-merge';
+let firstObject = {"a": 1, "b": {"c":2, "d":3}}
+let secondObject = {"a": 1};
+let result = merge(fisrtObject, secondObject, {diff:true});
+console.log(result === firstObject); // false
+console.log(result === secondObject); // false
+console.log(result); // {"a": 1}
+```
+
+
+  
+
